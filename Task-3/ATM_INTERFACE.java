4f64bd68d64b84a5ddd63415b38dfd251db2e02e
navnathjadhav08 <navnathjadhav0824>
2023-10-10 23:35:15 +0530

    up to date

--- Task-3/ATM_INTERFACE.java
+++ Task-3/ATM_INTERFACE.java
@@ -1,19 +1,39 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Class Name:          ATM_INTERFACE
+//  Description:         This program implements a simple ATM (Automated Teller Machine)
+//                       interface allowing users to check balance, withdraw money, and
+//                       deposit money. The interface is text-based with colored text
+//                       using ANSI escape codes for a better user experience. It includes
+//                       a menu system with options for different banking operations.
+//  Author:              Navnath Jadhav
+//  Date:                03-10-2023
+//  Update Date:         10-10-2023
+//
+//////////////////////////////////////////////////////////////////////////
+
 import java.util.Scanner;
 
 public class ATM_INTERFACE {
     public static void main(String[] args) {
-        BankAccount userAccount = new BankAccount(1000.0); // Initial balance ₹1000.0
+        // Initialize user's bank account with an initial balance of ₹1000.0
+        BankAccount userAccount = new BankAccount(1000.0);
+        // Create an ATM object with the user's bank account
         ATM atm = new ATM(userAccount);
 
+        // Main ATM interface loop
         while (true) {
+            // Print the ATM menu
             printMenu();
             Scanner scanner = new Scanner(System.in);
             System.out.print("\u001B[36mEnter your choice: \u001B[0m"); // Cyan color for input text
             int choice = scanner.nextInt();
+            // Process the user's choice
             atm.processOption(choice);
         }
     }
 
+    // Method to print the ATM menu
     private static void printMenu() {
         System.out.println("\u001B[33m*****************************\u001B[0m"); // Yellow color for border
         System.out.println("\u001B[33m**      \u001B[32mWelcome to ATM\u001B[33m      **\u001B[0m"); // Green color for header
@@ -26,21 +46,27 @@ public class ATM_INTERFACE {
     }
 }
 
+// Class representing the user's bank account
 class BankAccount {
-    private double balance;
+    private double balance; // Stores the account balance
 
+    // Constructor to initialize the account with an initial balance
     public BankAccount(double initialBalance) {
         this.balance = initialBalance;
     }
 
+    // Method to get the account balance
     public double getBalance() {
         return balance;
     }
 
+    // Method to deposit money into the account
     public void deposit(double amount) {
         balance += amount;
     }
 
+    // Method to withdraw money from the account
+    // Returns true if withdrawal is successful, false if there are insufficient funds
     public boolean withdraw(double amount) {
         if (balance >= amount) {
             balance -= amount;
@@ -51,42 +77,54 @@ class BankAccount {
     }
 }
 
+// Class representing the ATM machine
 class ATM {
-    private BankAccount account;
+    private BankAccount account; // Reference to the user's bank account
 
+    // Constructor to initialize the ATM with a user's bank account
     public ATM(BankAccount account) {
         this.account = account;
     }
 
+    // Method to process the user's choice and perform corresponding actions
     public void processOption(int option) {
         Scanner scanner = new Scanner(System.in);
-        double amount;
+        double amount; // Variable to store the transaction amount entered by the user
+
+        // Switch statement to handle different user choices
         switch (option) {
             case 1:
+                // Display user's balance
                 System.out.println("\u001B[32mYour balance: ₹" + account.getBalance() + "\u001B[0m"); // Green color for balance
                 break;
             case 2:
+                // Withdraw money
                 System.out.print("\u001B[36mEnter amount to withdraw: ₹\u001B[0m"); // Cyan color for input text
                 amount = scanner.nextDouble();
                 if (account.withdraw(amount)) {
+                    // Withdrawal successful
                     System.out.println("\u001B[32mWithdrawal successful. Remaining balance: ₹" + account.getBalance() + "\u001B[0m"); // Green color for success message
                 } else {
+                    // Insufficient funds for withdrawal
                     System.out.println("\u001B[31mInsufficient funds. Withdrawal failed.\u001B[0m"); // Red color for error message
                 }
                 break;
             case 3:
+                // Deposit money
                 System.out.print("\u001B[36mEnter amount to deposit: ₹\u001B[0m"); // Cyan color for input text
                 amount = scanner.nextDouble();
                 account.deposit(amount);
+                // Deposit successful
                 System.out.println("\u001B[32mDeposit successful. Updated balance: ₹" + account.getBalance() + "\u001B[0m"); // Green color for success message
                 break;
             case 4:
+                // Exit the ATM interface
                 System.out.println("\u001B[32mThank you for using the ATM. Goodbye!\u001B[0m"); // Green color for exit message
                 System.exit(0);
                 break;
             default:
+                // Invalid option
                 System.out.println("\u001B[31mInvalid option. Please try again.\u001B[0m"); // Red color for error message
         }
     }
 }
-
--- Task-3/README.md
+++ Task-3/README.md
@@ -1,2 +1,79 @@
-# CodeSoft_Internship
-Welcome to the codesoft_internship repository! 🚀  This repository serves as a comprehensive compilation of the incredible programming journey undertaken during the internship at Codesoft Solutions. Here, you will find  innovative projects, and invaluable learning experiences, all meticulously documented for your perusal.
+# ATM Interface
+
+This is a simple ATM (Automated Teller Machine) interface implemented in Java. The program allows users to check their account balance, withdraw money, and deposit money.
+
+## Features
+
+- **Check Balance:** Users can check their account balance.
+- **Withdraw:** Users can withdraw money from their account.
+- **Deposit:** Users can deposit money into their account.
+
+## Getting Started
+
+### Prerequisites
+
+- Java Development Kit (JDK) installed on your system.
+
+### Running the Program
+
+1. Clone the repository to your local machine:
+
+   ```
+   git clone https://github.com/navanathjadhav08/atm_interface.git
+   ```
+
+2. Compile the Java files:
+
+   ```
+   javac ATM_INTERFACE.java
+   ```
+
+3. Run the program:
+
+   ```
+   java ATM_INTERFACE
+   ```
+
+## Usage
+
+1. Choose an option from the menu:
+   - Press `1` to check your balance.
+   - Press `2` to withdraw money.
+   - Press `3` to deposit money.
+   - Press `4` to exit.
+
+2. Follow the prompts to enter the necessary information.
+
+## Example
+
+```
+*****************************
+**      Welcome to ATM      **
+*****************************
+1. Check Balance
+2. Withdraw
+3. Deposit
+4. Exit
+*****************************
+
+Enter your choice: 1
+Your balance: ₹1000.0
+
+Enter your choice: 2
+Enter amount to withdraw: ₹500
+Withdrawal successful. Remaining balance: ₹500.0
+
+Enter your choice: 3
+Enter amount to deposit: ₹200
+Deposit successful. Updated balance: ₹700.0
+
+Enter your choice: 4
+Thank you for using the ATM. Goodbye!
+```
+
+## Authors
+
+- [Navnath JAdhav](https://github.com/navnathjadhav08)
+
+
+
